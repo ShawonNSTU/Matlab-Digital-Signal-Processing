5c02f600f9d789d1f3794b676e66668ceaf7affc
ShawonNSTU <shuvroshawon9@gmail.com>
2018-07-02 00:41:58 +0600

    Add files via upload

---
+++ unit_sample_sequence.m
@@ -0,0 +1,19 @@
+clc;
+n=-20:20;                           % The colon operator (first:last) generates a 1-by-n matrix (or vector) of sequential numbers
+                                    % from the first value to the last.
+                                    % The default sequence is made up of incremental values, each 1 greater than the previous one:
+                                    % A = 10:15
+                                    % A = 10    11    12    13    14    15       
+                                    
+L=length(n);    
+for i=1:L
+    if n(i)==0
+        x(i)=1;
+    else
+        x(i)=0;
+    end;
+end;
+stem(n,x);
+xlabel('time n ------>');
+ylabel('amplitude ------>');
+title('unit ramp signal');
